bbddc71a5a620ab4d5f694a2765f83a5a904e60b
priyanshu goel <priyanshugoel001@gmail.com>
2021-07-04 04:17:59 +0530

    firebase-sign-in

---
+++ src/Components/Firebase.js
@@ -0,0 +1,14 @@
+  
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+firebase.initializeApp(
+{
+        apiKey: "AIzaSyA3o6I8mDupor5J5qcJWMMad06kyWAqQ9w",
+        authDomain: "class-demo-dce46.firebaseapp.com",
+        projectId: "class-demo-dce46",
+        storageBucket: "class-demo-dce46.appspot.com",
+        messagingSenderId: "39798573825",
+        appId: "1:39798573825:web:f4747e038723f74e082900"
+})
+export default firebase;
